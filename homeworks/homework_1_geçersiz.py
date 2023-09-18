de3a9ceddf30235b5d5bb410321e49173e6cf19f
Zeynep <135327034+suzzyeng@users.noreply.github.com>
2023-09-19 01:05:23 +0300

    Create homework_1_geçersiz.py

---
+++ homeworks/homework_1_geçersiz.py
@@ -0,0 +1,17 @@
+## 5 GEÇERSİZ STRİNGS
+
+# 1.örnek [Çift tırnak ya da tek tırnak işareti kullanılarak tanımlanmamış.]
+z
+print(z)
+
+# 2.örnek [Değişken ismi sayı ile başlayamaz.]
+1okunusu="bir"
+
+# 3.örnek [Alt tire dışında özel karakter kullanılamaz. - kullanılmış]
+sınıf-subesı="10-C"
+
+# 4.örnek [Boşluk içeremez]
+ no="0000-000-00-00"
+
+# 5.örnek [Komut isimleriyle tanımlama yapılamaz.]
+if="Spotify"
